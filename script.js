f886cb4e4b0a87fa890b7a87cb046ea418a3523e
Abdelrahman Mohamed <91153013+Abd-elrahman-Mohamed@users.noreply.github.com>
2025-04-18 16:01:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,51 +11,106 @@ const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
 const numberChars = "0123456789";
 const symbolChars = "!@#$%^&*()_+=-[]{};:<>?/";
 
-function savePasswordToLocalStorage(password) {
-  localStorage.setItem("password", JSON.stringify(password));
-}
+generateBtn.addEventListener("click", generatePassword);
+window.addEventListener("load", () => {
+  lengthInput.value = 8;
+  upperCase.checked = true;
+  numbers.checked = true;
+  generatePassword();
+});
 
-generateBtn.addEventListener("click", () => {
-  let length = +lengthInput.value;
-  let validChars = "";
+function generatePassword() {
+  let length = lengthInput.value;
   let password = "";
-
+  let validChars = "";
+  if (length > 16 || length < 8) {
+    display.value = `Please Enter The Length Between 8 to 16 Maximum`;
+    return;
+  }
   upperCase.checked ? (validChars += upperLetters) : "";
   lowerCase.checked ? (validChars += lowerLetters) : "";
   numbers.checked ? (validChars += numberChars) : "";
   symbols.checked ? (validChars += symbolChars) : "";
-
-  if (!length) {
-    display.value = "Enter Password's Length";
+  if (validChars === "") {
+    display.value = `Just Set The Setting to Get Password`;
     setTimeout(() => {
       display.value = "";
-    }, 1000);
+    }, 500);
     return;
   }
-
-  if (validChars === "") {
-    display.value = "Setup The Full Setting First";
-    return;
-  }
-
   for (let i = 0; i < length; i++) {
-    let randomIndex = Math.floor(Math.random() * validChars.length);
-    password += validChars[randomIndex];
+    password += validChars.at(Math.floor(Math.random() * validChars.length));
   }
-
   display.value = password;
-  savePasswordToLocalStorage(password);
-});
+}
 
-display.addEventListener("click", () => {
-  display.select();
-  document.execCommand("copy");
-  display.value = "Copied";
-  setTimeout(() => {
-    display.value = JSON.parse(localStorage.getItem("password")) || "";
-  }, 700);
-});
+/* 
 
-window.addEventListener("load", () => {
-  display.value = JSON.parse(localStorage.getItem("password")) || "";
-});
+.1
+.9
+*/
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// generateBtn.addEventListener("click", () => {
+//   let length = +lengthInput.value;
+//   if (length < 16 && length > 8) {
+//     display.value = `Max Length is 16\nMin Length is 8`;
+//     return;
+//   }
+//   let validChars = "";
+//   let password = "";
+
+//   upperCase.checked ? (validChars += upperLetters) : "";
+//   lowerCase.checked ? (validChars += lowerLetters) : "";
+//   numbers.checked ? (validChars += numberChars) : "";
+//   symbols.checked ? (validChars += symbolChars) : "";
+
+//   if (!length) {
+//     display.value = "Enter Password's Length";
+//     setTimeout(() => {
+//       display.value = "";
+//     }, 1000);
+//     return;
+//   }
+
+//   if (validChars === "") {
+//     display.value = "Setup The Full Setting First";
+//     return;
+//   }
+
+//   for (let i = 0; i < length; i++) {
+//     let randomIndex = Math.floor(Math.random() * validChars.length);
+//     password += validChars[randomIndex];
+//   }
+
+//   display.value = password;
+//   savePasswordToLocalStorage(password);
+// });
+
+// display.addEventListener("click", () => {
+//   display.select();
+//   document.execCommand("copy");
+//   display.value = "Copied";
+//   setTimeout(() => {
+//     display.value = JSON.parse(localStorage.getItem("password")) || "";
+//   }, 700);
+// });
+
+// window.addEventListener("load", () => {
+//   display.value = JSON.parse(localStorage.getItem("password")) || "";
+// });
